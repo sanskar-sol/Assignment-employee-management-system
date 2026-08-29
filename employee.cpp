0e39275455ed487df27a5618f4e173f139fb5415
sanskar-sol <sanskarvirat3@gmail.com>
2026-08-29 20:48:07 +0530

    Update .gitignore and refactor employee classes
    - Added *.out to .gitignore to exclude output files.
    - Changed employee_id, government_id, and date_of_hire to non-const in EmployeeIdentity class.
    - Implemented EmployeeProfile class with getter and setter methods for department, salary, and job title.
    - Updated print_date function to use 'cout' instead of 'std::cout'.

--- employee.cpp
+++ employee.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 struct Date
 {
     int day;
@@ -15,9 +17,9 @@ class Employee{};
 class EmployeeIdentity
 {
     private:
-        const int employee_id;
-        const int government_id;
-        const Date date_of_hire;
+        int employee_id;
+        int government_id;
+        Date date_of_hire;
 
     public:
         EmployeeIdentity(int emp_id, int gov_id, Date date)
@@ -26,31 +28,78 @@ class EmployeeIdentity
             
         }
 
-        int get_employee_id()
+        // getter metnods 
+        int get_employee_id() const
         {
             return employee_id;
         }
 
-        int get_government_id()
+        int get_government_id() const 
         {
             return government_id;
         }
 
-        Date get_date_of_hire()
+        Date get_date_of_hire() const
         {
             return date_of_hire;
         }
+};  
+
+class EmployeeProfile
+{
+    private:
+        mutable string department;
+        mutable long salary;
+        mutable string job_title;
+
+    public:
+        EmployeeProfile(string dep, long sal, string job)
+            : department(dep), salary(sal), job_title(job)
+        {
+        
+        }
+
+        // getter methods 
+        long get_salary() const
+        {
+            return salary;
+        }
+
+        string get_department() const
+        {
+            return department;
+        }
+
+        string get_job_title() const
+        {
+            return job_title;
+        }
+
+        // setter methods
+        void set_salary(int sal)
+        {
+            salary = sal;
+        }
+
+        void set_department(string dep)
+        {
+            department = dep;
+        }
+
+        void set_job_title(string job)
+        {
+            job_title = job;
+        }
 };
-
-
 // helper function to print a date 
 
 void print_date(Date date)
 {
-    std::cout << date.day << "/" << date.month << "/" << date.year << std::endl;
+    cout << date.day << "/" << date.month << "/" << date.year << std::endl;
 }
+
 
 int main()
 {
-
+    return 0;
 }
